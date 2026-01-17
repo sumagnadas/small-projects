389a6a6c8059dd60626b7ae35126fd7fd5b71557
sumagnadas <sumagnadas@gmail.com>
2026-01-18 03:44:39 +0530

    Added functionalities for creating a socket, binding to it and listening for incoming connections.

--- web-server/main.s
+++ web-server/main.s
@@ -2,8 +2,57 @@
 .global _start
 
 exit:
-        mov rax, 60
-        syscall
-        ret # just for the sake of it
+	mov rax, 60
+	syscall
+	ret # just for the sake of it
+socket:
+	mov rax, 41
+	syscall
+	ret
+
+bind:
+	mov rax, 49
+	syscall
+	ret
+listen:
+	mov rax, 50
+	syscall
+	ret
 _start:
-        call exit
+	# create a socket
+	mov rdi, 2
+	mov rsi, 1
+	mov rdx, 0
+	call socket 
+
+	# creating the sockaddr object on stack
+	push rbp
+	mov rbp, rsp
+	sub rbp, 8
+	sub rsp, 25 # 16 for sockaddr + 8 for the initial offset
+	
+	mov byte ptr [rbp], al # store the fd on the stack
+	# sockaddr object
+	mov word ptr [rbp+1], 0x0002        # sin_family
+	mov word ptr [rbp+3], 0x5000      # sin_port
+	mov dword ptr [rbp+5], 0x00000000 # sin_addr
+	
+	# bind the socket
+	mov dil, byte ptr [rbp]  # move the socket fd to rdi
+	lea rsi, [rbp+1]
+	mov rdx, 16
+	call bind
+	
+	# listen on the bound port
+	mov dil, byte ptr [rbp]
+	mov rsi, 0
+	call listen
+
+	# return the stack to previous point
+	add rbp, 8
+	mov rsp, rbp
+	pop rbp
+	
+	# exit the program
+	mov rdi, 0
+	call exit
