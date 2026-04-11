548e78cf31e0b63199c891ec2a8beb83ecca8b63
sumagnadas <sumagnadas@gmail.com>
2026-04-11 22:40:29 +0530

    Reduced jump overheads due to function calls.

--- web-server/main.s
+++ web-server/main.s
@@ -1,6 +1,6 @@
 .intel_syntax noprefix
 .data
-	header: 
+	header:
 		.asciz "HTTP/1.0 200 OK\r\n\r\n"
 	get_st:  .ascii "GET "
 	post_st:  .asciz "POST"
@@ -14,49 +14,13 @@
 .text
 .global _start
 
-exit:
-	mov rax, 60
-	syscall
-	ret # just for the sake of it
-socket:
-	mov rax, 41
-	syscall
-	ret
-
-bind:
-	mov rax, 49
-	syscall
-	ret
-listen:
-	mov rax, 50
-	syscall
-	ret
-accept:
-	mov rax, 43
-	syscall
-	ret
-open:
-	mov rax, 2
-	syscall
-	ret
-write:
-	mov rax, 1
-	syscall
-	ret
-close:
-	mov rax, 3
-	syscall
-	ret
-read:
-	mov rax, 0
-	syscall
-	ret
 _start:
 	# create a socket
 	mov rdi, 2
 	mov rsi, 1
 	mov rdx, 0
-	call socket
+	mov rax, 41
+	syscall
 
 	### Even though stack grows downwards,
 	### data writing and reading is always done
@@ -77,18 +41,21 @@ _start:
 	mov rdi, qword ptr [socket_fd]  # move the socket fd to rdi
 	lea rsi, [rbp-16]
 	mov rdx, 16
-	call bind
+	mov rax, 49
+	syscall
 
 	# listen on the bound port
 	mov rdi, qword ptr [socket_fd]
 	mov rsi, 0
-	call listen
+	mov rax, 50
+	syscall
 
 	# accept incoming connections
 	mov rdi, qword ptr [socket_fd]
 	mov rsi, 0
 	mov rdx, 0
-	call accept
+	mov rax, 43
+	syscall
 
 	# store the fd for the connection
 	mov word ptr [conn_fd], ax
@@ -97,9 +64,10 @@ _start:
 	mov rdi, qword ptr [conn_fd]
 	lea rsi, [request_data]
 	mov rdx, 256
-	call read
+	mov rax, 0
+	syscall
 
-	mov eax, dword ptr [get_st] 
+	mov eax, dword ptr [get_st]
 	cmp dword ptr [request_data], eax
 	je GET
 
@@ -127,7 +95,8 @@ _start:
 		# open the file requested
 		lea rdi, [filename]
 		mov rsi, 0
-		call open
+		mov rax, 2
+		syscall
 
 		mov word ptr [file_fd], ax # store the fd
 
@@ -135,36 +104,43 @@ _start:
 		mov rdi, qword ptr [file_fd]
 		lea rsi, [file_data]
 		mov rdx, 256
-		call read
+		mov rax, 0
+		syscall
 
 		# read the file data size
 		mov rbx, rax
-		call close
+		
+		# close the fd
+		mov rax, 3
+		syscall
 		
 		# send the header
 		mov rdi, qword ptr [conn_fd]
-		lea rsi, header 
+		lea rsi, header
 		mov rdx, 19
-		call write
+		mov rax, 1
+		syscall
 		
 		# send the file data
 		mov rdi, qword ptr [conn_fd]
 		lea rsi, [file_data]
 		mov rdx, rbx
-		call write
+		mov rax, 1
+		syscall
 
 		jmp done
 	POST:
 		mov rdi, rbx
-		lea rsi, header 
+		lea rsi, header
 		mov rdx, 19
-		call write
-
+		mov rax, 1
+		syscall
 	done:
 	# close the conenction after sending the response
 		mov rdi, 0
-		mov di, word ptr [conn_fd]
-		call close
+		mov rdi, qword ptr [conn_fd]
+		mov rax, 3
+		syscall
 
 	# return the stack to previous point
 	mov rsp, rbp
@@ -172,4 +148,5 @@ _start:
 
 	# exit the program
 	mov rdi, 0
-	call exit
+	mov rax, 60
+	syscall
